347fa696bec988a1090ebd0146b2c44ff95a5681
CedricChia123 <cedricchia01@gmail.com>
2023-02-15 00:43:15 +0800

    Update problem 1 and problem 2

---
+++ src/problem1/solution.js
@@ -0,0 +1,19 @@
+var sum_to_n_a = function(n) {
+    let sum = 0;
+    for (let i = 1; i < n + 1; i++) {
+        sum += i
+    }
+    return sum;
+};
+
+var sum_to_n_b = function(n) {
+    if (n == 0) {
+        return 0;
+    } else {
+        return sum_to_n_b(n - 1) + n;
+    }
+};
+
+var sum_to_n_c = function(n) {
+    return (n * (n + 1)) / 2;
+};
---
+++ src/problem2/script.js
@@ -0,0 +1,28 @@
+const addr = document.getElementById("input-address");
+const amount = document.getElementById("input-amount");
+const otp = document.getElementById("input-otp");
+const form = document.getElementById("form");
+
+form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  if (!addr.value) {
+    alert("Please enter an address to transfer to")
+  } else if (!amount.value) {
+    alert("Please key in an amount to transfer")
+  } else if (!otp.value) {
+    alert("Please key in the OTP given to you")
+  } else {
+    openPopup();
+  }
+})
+
+function openPopup(){
+  var popup = document.getElementById("popup");
+  popup.classList.add("show-popup");
+}
+
+function closePopup(){
+  var popup = document.getElementById("popup");
+  popup.classList.remove("show-popup");
+  form.submit();
+}
